e36bb7e93769c50a671625c7fee56bdc0d421d7f
stappost <emanuele.scarpantoni@gmail.com>
2023-12-02 11:50:43 +0100

    add levels

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -1,8 +1,8 @@
 // creare funzione che genera 5 numeri casuali;
 //  inserire 5 numeri unici in un array;
-function num_random(){
+function num_random(quantity){
     let num_array = [];
-    while(num_array.length<5){
+    while(num_array.length<quantity){
         let num = Math.floor(Math.random() *100 +1);
         if(!num_array.includes(num)){
             num_array.push(num)
@@ -18,42 +18,67 @@ function list(num){
 
     let ul = document.getElementById("list_number");
     let li = document.createElement("li");
-    li.innerText = num
-    ul.appendChild(li)
+    li.innerText = num;
+    ul.appendChild(li);
 }
 
-let num = num_random();
-
-for(let i = 0; i<5; i++){
-    list(num[i])
+function num_to_memorize(){
+    let level = document.getElementById("difficulty").value;
+    let quantity
+    switch(level){
+        case "1":
+            quantity = 5;
+            break;
+        case "2":
+            quantity = 10;
+            break;
+        case "3":
+            quantity = 15;
+            break;
+        default:
+            return;    
+    }
+    let num = num_random(quantity)
+    for(let i = 0; i< quantity; i++){
+        list(num[i])
+    }
+    return quantity
 }
-// dopo 30 secondi far scomparire i numeri;
 
-setTimeout(function(){
-    document.getElementById("list_number").classList.add("d-none")
-}, 3000)
-// dopo 32 secondi richiedere all'utente, con 5 promp, i numeri visualizzati;
-setTimeout(function(){
-    // inizializzare variabile dei numeri indovinati;
-    let guessed = 0;
-    // inizializzare array right;
-    let right = [];
-    let number;
-    for(let i = 0 ; i < 5 ; i++){
-        number = parseInt(prompt("inserire un numero tra i visualizzati"));
-        // ? il numero inserito è presente nell'array
-        //  incrementare variabile indovinati;
-        //  aggiungere il numero all'interno dell'array right;
-        if(num.includes(number)){
-            right.push(number);
-            guessed++;
-        }
-    } 
-    // stampare variabile numeri indovinate;
-    //  stampare array right;
-    console.log(guessed);
-    console.log(`hai indovinato i seguenti numeri: ${right}`);
-}, 3200)
+
+const btn_play = document.getElementById("play")
+
+btn_play.addEventListener("click", function(){
+    let quantity = num_to_memorize();
+    
+    // dopo 30 secondi far scomparire i numeri;
+
+    setTimeout(function(){
+        document.getElementById("list_number").classList.add("d-none")
+    }, 3000)
+    // dopo 32 secondi richiedere all'utente, con 5 promp, i numeri visualizzati;
+    setTimeout(function(){
+        // inizializzare variabile dei numeri indovinati;
+        let guessed = 0;
+        // inizializzare array right;
+        let right = [];
+        let number;
+        for(let i = 0 ; i < quantity ; i++){
+            number = parseInt(prompt("inserire un numero tra i visualizzati"));
+            // ? il numero inserito è presente nell'array
+            //  incrementare variabile indovinati;
+            //  aggiungere il numero all'interno dell'array right;
+            if(num_random(quantity).includes(number)){
+                right.push(number);
+                guessed++;
+            }
+        } 
+        // stampare variabile numeri indovinate;
+        //  stampare array right;
+        console.log(guessed);
+        console.log(`hai indovinato i seguenti numeri: ${right}`);
+    }, 3200)
+})
 
 
 
